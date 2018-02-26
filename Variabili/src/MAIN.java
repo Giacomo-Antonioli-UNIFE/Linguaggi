1fbc72d9d2d6b736b63ca69b4e8a8c8e09e8a42d
Giacomo <giacomo.antonioli@student.unife.it>
2018-02-26 17:34:13 +0100

    First Update
    First programmes in java

---
+++ Variabili/src/MAIN.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+
+/**
+ * @author Giacomo
+ *
+ */
+public class MAIN {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=4, b=5 ;
+		int c = a+b;
+		System.out.println(a+"+"+b+"="+c);
+		
+	}
+
+}
